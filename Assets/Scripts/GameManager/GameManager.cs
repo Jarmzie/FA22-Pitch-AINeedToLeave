6e445a71f6f1844882e5c86d3dd46d037eb80890
toppop7 <65695818+toppop7@users.noreply.github.com>
2022-09-18 15:57:36 -0500

    Basic UI for collectables

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -6,11 +6,17 @@ public class GameManager: MonoBehaviour
 {
     public static GameManager Instance;
     public int CollectGotten;
+    public UIManager UIM;
     private void Awake(){
         if(Instance){
             Debug.Log("GameManager already exists.");
         }
         Instance = this;
+
+        UIM = GameObject.Find("Canvas").GetComponent<UIManager>();
+        Debug.Log(UIM.ToString());
     }
+
+  
 
 }
--- Assets/Scripts/Pickups/Pickup_Collectable.cs
+++ Assets/Scripts/Pickups/Pickup_Collectable.cs
@@ -6,5 +6,6 @@ public class Pickup_Collectable : PickupBase
 {
     public override void PickupBehavior(){
       GameManager.Instance.CollectGotten += 1;
+      GameManager.Instance.UIM.ColText.text = "" + GameManager.Instance.CollectGotten;
     }
 }
